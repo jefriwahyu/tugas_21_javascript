13de63e6a2c9f1c68807fcf03aeb8b17f25331e1
Jefriwahyu <memberjefry1@gmail.com>
2022-03-08 10:59:11 +0700

    Tugas 21 Javascript
    Tugas ini saya kumpulkan untuk NIOMIC.

---
+++ index.js
@@ -0,0 +1,8 @@
+function cooking(){
+  var nasgor = "Saya belajar masak nasi goreng";
+  console.log(nasgor);
+  var eat = nasgor.replace("masak", "makan");
+  return eat;
+}
+
+console.log(cooking());
